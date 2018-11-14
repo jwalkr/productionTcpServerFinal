80fe8c197ee638018d310f6fbfbbae176256f63f
KatlegoRatau <katlegoratau@thedigitalacademy.co.za>
2018-11-14 14:58:23 +0200

    Stable version

--- TCPServer.js
+++ TCPServer.js
@@ -13,7 +13,7 @@ const token = '<?xml version="1.0" encoding="ISO-8859-1"?> <cookie VALUE="UXCKB1
 const msgPDU = `<?xml version="1.0" encoding="ISO-8859-1"?>
 <ussd
 PDU="PSSRR"
-MSISDN="27832129180"
+MSISDN="27788425401"
 STRING="*121#"
 TID="1034"
 REQID="0"
@@ -27,68 +27,152 @@ BRAND="TIKTAK"
 <cookie/>
 </ussd>`
 
+ const contentReply = `<?xml version="1.0" encoding="ISO-8859-1"?>
+ <ussd
+ PDU="USSRC"
+ MSISDN="27788425401"
+ STRING="0"
+ TID="1034"
+ REQID="0"
+ TARIFF="*">
+ <attributes
+ IMSI=
+ "655101234567890"
+ SUBTYPE="P"
+ BRAND="TIKTAK"
+ />
+ <cookie/>
+ </ussd>` 
+
+ const numberReply = `<?xml version="1.0" encoding="ISO-8859-1"?>
+ <ussd
+ PDU="USSRC"
+ MSISDN="27788425401"
+ STRING="27788425401"
+ TID="1034"
+ REQID="0"
+ TARIFF="*">
+ <attributes
+ IMSI=
+ "655101234567890"
+ SUBTYPE="P"
+ BRAND="TIKTAK"
+ />
+ <cookie/>
+ </ussd>` 
+
+
+
+
+
+
+
+// const kue = require('kue')
+// const jobs = kue.createQueue()
+
+
+
+// function newJob(){
+//     var job = jobs.create('new_job')
+//     job.save()
+// }
+
+// jobs.process('new_job' , function(job , done){
+    
+//     console.log('Job' , job.id, 'is done')
+//     done && done()
+// })
+
+
+// setInterval(newJob , 3000)
+
+
 
 net.createServer(function(sock) {
 
-    sock.on('data', function(data) {
-
-        buff = Buffer.from(data);
-
-
-        if(data)
-        {
-            //Regex
-            
-            if(buff.toString().search('<login COOKIE="ussdgw" NODE_ID="MTNMENU_F02" PASSWORD="mtnm3nu123" RMT_SYS="uxml@ussdgw" USER="MTNMENU_F02"/>'))
+        sock.on('data', function(data) {
+    
+            buff = Buffer.from(data);
+    
+    
+            if(data)
             {
-
-                    
-                    sock.write(token) ;                    
-                    sock.write(Buffer.from('ff', 'hex'));
-                    //sock.destroy();
-
-                    sock.on("data", info => {
-                            console.log("================");
-                    
-                            let hasSenWritten =sock.write(msgPDU);
-                            let hasSendPDU = sock.write(Buffer.from('ff', 'hex'));
-                            
-                            if(hasSenWritten)
-                            {
-                                console.log("Check PDU message sending================33");
-
-                                console.log(hasSenWritten);
+                //Regex
+                
+                if(buff.toString().search('<login COOKIE="ussdgw" NODE_ID="MTNMENU_F02" PASSWORD="mtnm3nu123" RMT_SYS="uxml@ussdgw" USER="MTNMENU_F02"/>'))
+                {
+    
+                        
+                        sock.write(token) ;                    
+                        sock.write(Buffer.from('ff', 'hex'));
+                        //sock.destroy();
+    
+                        sock.on("data", pduReply => {
+                                console.log("================");
+                        
+                                let hasWritten =sock.write(msgPDU);
+                                let hasSendPDU = sock.write(Buffer.from('ff', 'hex'));
                                 
-                                sock.pause();
-
-                            }
-
-                            
-                            
-
-                         
-
-                 })
-                    
-
-
-
-
+                                
+                                if(hasWritten)
+                                {
+                                    console.log("Check PDU message sending================33");
+    
+                                    // sock.write(msgPDU);
+                                    console.log(pduReply.toString());
+                                    sock.write(numberReply);
+                                    sock.write(Buffer.from('ff', 'hex'));
+    
+                                  
+    
+                                            // sock.on("data", d5 =>{
+    
+                                            //     console.log(d5);
+                                            // });
+    
+                                            
+                                  
+                                  
+                                    
+                                    
+    
+                                    sock.pause();
+    
+                                }
+    
+                                
+                                
+    
+                             
+    
+                     })
+                        
+    
+    
+    
+    
+               
+                      
+                       
+                }
+    
+            }  
+            sock.setKeepAlive(true)
+    
            
-                  
-                   
-            }
+            
+        })
+    
+    }).listen(PORT,HOST)
+    
+    console.log("Server listening on "+HOST+":"+PORT);
 
-        }  
-        sock.setKeepAlive(true)
+ 
 
-       
-        
-    })
 
-}).listen(PORT,HOST)
 
-console.log("Server listening on "+HOST+":"+PORT);
+
+
 
 
 // //receive the socket and the message PDU *121#
