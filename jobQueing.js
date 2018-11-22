e878b5e9da1778e897abd15dc6126d3b36f05f07
jwalkr <johnkyony@thedigitalacademy.co.za>
2018-11-22 20:38:30 +0200

    server rewrite

--- jobQueing.js
+++ jobQueing.js
@@ -7,6 +7,61 @@ const bodyParse =require('body-parser')
 const app = express()
 const PORT = 5000
 
+//server connection
+
+const host = process.env.HOST||'127.0.0.1'
+const port = process.env.PORT||8000;
+
+//tcp variables
+let buff = null 
+let buffRespond = null 
+const token = '<?xml version="1.0" encoding="ISO-8859-1"?> <cookie VALUE="UXCKB1TAIS7XT6"/>'
+let userRequestJob = null
+
+
+
+const server = net.createServer((socket) => {
+    socket.on('data' , (loginToken) => {
+        // check if we receiving wasp credentials 
+        console.log('Response:' +  loginToken)
+        //stream data into the buffer 
+        buff = Buffer.from(loginToken)
+        // check if there is data in the pipe
+        if(loginToken){
+            //search for the login request in the buffer
+            if(buff.toString().search('<login COOKIE="ussdgw" NODE_ID="MTNMENU_F02" PASSWORD="mtnm3nu123" RMT_SYS="uxml@ussdgw" USER="MTNMENUF02"/>')){
+                socket.write(loginToken)
+                socket.write(Buffer.from('ff' , 'hex'))
+            }
+        }
+    })
+    .on('connect' , (connectionConfirmation) =>{
+        console.log('Connected and ready to receive jobs')
+        console.log(connectionConfirmation.toString())
+    })
+
+
+})
+
+server.listen(8000 , '127.0.0.1')
+
+
+// socket.on('data' , (serverResponse) => {
+//     //check if we are receiving any data 
+//     console.log('RESPONSE:' + data)
+//     //stream data into the buffer 
+//     buff = Buffer.from(data)
+//     //check if there is data in the pipe 
+//     if(data){
+//          //search for the login request in the buffer
+//          if(buff.toString().search('<login COOKIE="ussdgw" NODE_ID="MTNMENU_F02" PASSWORD="mtnm3nu123" RMT_SYS="uxml@ussdgw" USER="MTNMENUF02"/>')){
+//              socket.write(token)
+//              socket.write(Buffer.from('ff' , 'hex'))
+//          }
+//     }
+
+// })
+
 
 //job monitoring interface
 const kueUiExpress = require('kue-ui-express')
@@ -26,12 +81,6 @@ app.listen(PORT, () => {
 app.use(bodyParse.json())
 app.use(bodyParse.urlencoded({extended: false}))
 
-
-//tcp variables
-let buff = null 
-let buffRespond = null 
-const token = '<?xml version="1.0" encoding="ISO-8859-1"?> <cookie VALUE="UXCKB1TAIS7XT6"/>'
-let userRequestJob = null
 
 
 //Add a job
@@ -70,55 +119,28 @@ app.post('/api/v1/addJob' , (req,res)=>{
 
 queue.process('UserRequest', 10 ,function(job , done ){
 
-    const host = process.env.HOST||'127.0.0.1'
-    const port = process.env.PORT||8000;
-
-    const socket = net.createConnection(port , host)
+    
     console.log('socket created')
-    socket.on('data' , function(data){
-        //check if we are receiving any data 
-        console.log('RESPONSE:' + data)
-        //stream data into the buffer 
-        buff = Buffer.from(data)
-        //check if there is data in the pipe 
-        if(data){
-            //search for the login request in the buffer
-            if(buff.toString().search('<login COOKIE="ussdgw" NODE_ID="MTNMENU_F02" PASSWORD="mtnm3nu123" RMT_SYS="uxml@ussdgw" USER="MTNMENUF02"/>')){
-                socket.write(token)
+    console.log('Sending PSSSR')
+    server.write(job.data.msgPDU)
+    server.write(Buffer.from('ff' , 'hex')) 
+    
+    server.on('data' ,(pduReply) => {
+        server.write(job.data.contentReply)
+        server.write(Buffer.from('ff' , 'hex'))
+        if(job.data.numberReply != ''){
+            socket.on('data' , gateWayResponse => {
+                console.log(gateWayResponse.toString())
+                socket.write(job.data.numberReply)
                 socket.write(Buffer.from('ff' , 'hex'))
-            }
+            })
+
         }
-    })
-    .on('connect' , function(){
-        socket.on('data' , pduReply => {
-            console.log('Sending PSSSR')
-            let hasWritten = socket.write(job.data.msgPDU)
-            socket.write(Buffer.from('ff' , 'hex'))
+        console.log('The job has been completed')
+       
+        socket.pause()
 
-            if(hasWritten){
-                console.log("checking PDU message sending")
 
-                //writing action request in the pipe 
-                console.log(pduReply.toString())
-
-                socket.write(job.data.contentReply)
-                socket.write(Buffer.from('ff' , 'hex'))
-
-                if(job.data.numberReply != ''){
-                    socket.on('data' , gateWayResponse => {
-                        console.log(gateWayResponse.toString())
-                        socket.write(job.data.numberReply)
-                        socket.write(Buffer.from('ff' , 'hex'))
-                    })
-
-                }
-
-               
-                socket.pause()
-                socket.end()
-
-            }
-        })
     })
     .on('end' , function(){
         console.log('The Request has been Proccessed')
@@ -136,6 +158,10 @@ queue.process('UserRequest', 10 ,function(job , done ){
 
     })
 
+
+    
+
+    
 
 })
 
