6469b8a8860f4ace33b0270ee81d5b9cef9e6fa7
jwalkr <johnkyony@thedigitalacademy.co.za>
2018-11-23 10:28:56 +0200

    Connection to wasp rewrite

--- TCPServer.js
+++ TCPServer.js
@@ -9,56 +9,81 @@ let buff = null;
 let buffRespond = null;
 
 const token = '<?xml version="1.0" encoding="ISO-8859-1"?> <cookie VALUE="UXCKB1TAIS7XT6"/>';
-const msgPDU = `<?xml version="1.0" encoding="ISO-8859-1"?>
-<ussd
-PDU="PSSRR"
-MSISDN="27788425401"
-STRING="*121#"
-TID="1034"
-REQID="0"
-TARIFF="*">
-<attributes
-IMSI=
-"655101234567890"
-SUBTYPE="P"
-BRAND="TIKTAK"
-/>
-<cookie/>
-</ussd>`
+var HOST = process.env.HOST||'127.0.0.1';
+var PORT =  process.env.PORT||8000;
 
- const contentReply = `<?xml version="1.0" encoding="ISO-8859-1"?>
- <ussd
- PDU="USSRC"
- MSISDN="27788425401"
- STRING="0"
- TID="1034"
- REQID="0"
- TARIFF="*">
- <attributes
- IMSI=
- "655101234567890"
- SUBTYPE="P"
- BRAND="TIKTAK"
- />
- <cookie/>
- </ussd>` 
+const server = net.createServer((socket) => {
+    socket.on('data' , (loginToken) => {
+        // check if we receiving wasp credentials 
+        console.log('Response:' +  loginToken)
+        //stream data into the buffer 
+        buff = Buffer.from(loginToken)
+        // check if there is data in the pipe
+        if(loginToken){
+            //search for the login request in the buffer
+            if(buff.toString().search('<login COOKIE="ussdgw" NODE_ID="MTNMENU_F02" PASSWORD="mtnm3nu123" RMT_SYS="uxml@ussdgw" USER="MTNMENUF02"/>')){
+                socket.write(loginToken)
+                socket.write(Buffer.from('ff' , 'hex'))
+            }
+        }
+    })
+    .on('connect' , (connectionConfirmation) =>{
+        console.log('Connected and ready to receive jobs')
+        console.log(connectionConfirmation.toString())
+    })
 
- const numberReply = `<?xml version="1.0" encoding="ISO-8859-1"?>
- <ussd
- PDU="USSRC"
- MSISDN="27788425401"
- STRING="27788425401"
- TID="1034"
- REQID="0"
- TARIFF="*">
- <attributes
- IMSI=
- "655101234567890"
- SUBTYPE="P"
- BRAND="TIKTAK"
- />
- <cookie/>
- </ussd>` 
+
+})
+// const msgPDU = `<?xml version="1.0" encoding="ISO-8859-1"?>
+// <ussd
+// PDU="PSSRR"
+// MSISDN="27788425401"
+// STRING="*121#"
+// TID="1034"
+// REQID="0"
+// TARIFF="*">
+// <attributes
+// IMSI=
+// "655101234567890"
+// SUBTYPE="P"
+// BRAND="TIKTAK"
+// />
+// <cookie/>
+// </ussd>`
+
+//  const contentReply = `<?xml version="1.0" encoding="ISO-8859-1"?>
+//  <ussd
+//  PDU="USSRC"
+//  MSISDN="27788425401"
+//  STRING="0"
+//  TID="1034"
+//  REQID="0"
+//  TARIFF="*">
+//  <attributes
+//  IMSI=
+//  "655101234567890"
+//  SUBTYPE="P"
+//  BRAND="TIKTAK"
+//  />
+//  <cookie/>
+//  </ussd>` 
+
+//  const numberReply = `<?xml version="1.0" encoding="ISO-8859-1"?>
+//  <ussd
+//  PDU="USSRC"
+//  MSISDN="27788425401"
+//  STRING="27788425401"
+//  TID="1034"
+//  REQID="0"
+//  TARIFF="*">
+//  <attributes
+//  IMSI=
+//  "655101234567890"
+//  SUBTYPE="P"
+//  BRAND="TIKTAK"
+//  />
+//  <cookie/>
+//  </ussd>` 
 
 
 
@@ -102,55 +127,10 @@ const app = express()
 //Server Endpoint
 
 app.get('/api/v1/option1' , (req , res) => {
-    var HOST = process.env.HOST||'127.0.0.1';
-    var PORT =  process.env.PORT||8000;
-
-    var socket = net.createConnection(PORT , HOST)
+   
     console.log('Socket created.')
 
-    socket.on( 'data' , function(data) {
-        //check if we are receiving any data
-        console.log('RESPONSE:' + data)
-        //stream data into the buffer
-        buff = Buffer.from(data)
-        //check if there is data into the pipe 
-        if(data){
-            // search for the login request in the buffer
-            if(buff.toString().search('<login COOKIE="ussdgw" NODE_ID="MTNMENU_F02" PASSWORD="mtnm3nu123" RMT_SYS="uxml@ussdgw" USER="MTNMENU_FO2"/>')){
-                socket.write(token)
-                socket.write(Buffer.from('ff' , 'hex'))
-
-
-            }
-
-        }
-
-
-    }).on('connect' , function(){
-        socket.on('data' ,pduReply => {
-            console.log("Sending PSSSR")
-            let hasWritten = socket.write(msgPDU)
-            socket.write(Buffer.from('ff' , 'hex'))
-
-            //checking the pipeline
-            if(hasWritten){
-
-                console.log("Checking PDU message sending")
-
-
-                //writing action request in the pipe
-                console.log(pduReply.toString())
-                socket.write(numberReply)
-                socket.write(Buffer.from('ff' , 'hex'))
-                socket.on("data" , gateWayResponse => {
-                    console.log(gateWayResponse)
-                })
-            }
-        })
-
-    })
-
-
+   
 
 
 
