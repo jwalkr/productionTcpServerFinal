093d2859ab7ae918cd6b00172a4b860c8cf4dfd9
System Administrator <root@MacBook-Air.local>
2018-12-06 15:21:20 +0200

    added state

--- ServerTCP.js
+++ ServerTCP.js
@@ -2,89 +2,107 @@
 
 var net = require('net');
 
-var HOST = process.env.HOST||'127.0.0.1';
-var PORT =  process.env.PORT||8000;
+var HOST = process.env.HOST || '127.0.0.1';
+var PORT = process.env.PORT || 8000;
 
 let buff = null;
 
-let buffRespond = null;
+let hasLoggedIn = false;
+
 
 const token = '<?xml version="1.0" encoding="ISO-8859-1"?> <cookie VALUE="UXCKB1TAIS7XT6"/>';
 const msgPDU = `<?xml version="1.0" encoding="ISO-8859-1"?>
-<ussd
-PDU="PSSRR"
-MSISDN="27832129180"
-STRING="*121#"
-TID="1034"
-REQID="0"
-TARIFF="*">
-<attributes
-IMSI=
-"655101234567890"
-SUBTYPE="P"
-BRAND="TIKTAK"
-/>
-<cookie/>
-</ussd>`
+                <ussd
+                PDU="PSSRR"
+                MSISDN="27788425401"
+                STRING="*121#"
+                TID="1034"
+                REQID="0"
+                TARIFF="*">
+                <attributes
+                IMSI=
+                "655101234567890"
+                SUBTYPE="P"
+                BRAND="TIKTAK"
+                />
+                <cookie/>
+                </ussd>`
 
 
-net.createServer(function(sock) {
+net.createServer(function (sock) {
 
-   sock.on('data', function(data) {
+    sock.on('data', function (data) {
 
-       buff = Buffer.from(data);
+        buff = Buffer.from(data);
 
+        if(!hasLoggedIn)
+        {
 
-       if(data)
-       {
+            console.log("User not loged..");
+            
+            if (buff.toString().search('<login COOKIE="ussdgw" NODE_ID="MTNMENU_F02" PASSWORD="mtnm3nu123" RMT_SYS="uxml@ussdgw" USER="MTNMENU_F02"/>')) {
 
-           if(buff.toString().search('<login COOKIE="ussdgw" NODE_ID="MTNMENU_F02" PASSWORD="mtnm3nu123" RMT_SYS="uxml@ussdgw" USER="MTNMENU_F02"/>'))
-           {
-
-
-                   sock.write(token) ;
-                   sock.write(Buffer.from('ff', 'hex'));
-
-                   sock.on("data", info => {
-                           
-
-                           let hasSenWritten =sock.write(msgPDU);
-                           let hasSendPDU = sock.write(Buffer.from('ff', 'hex'));
-
-                           if(hasSenWritten)
-                           {
-                               console.log("Check PDU message sending================33");
-
-                               console.log(hasSenWritten);
-
-                               sock.pause();
-
-                           }
+                     hasLoggedIn = true;
+                     console.log("Loging in Now");
+                     console.log("Writing Token");
+                    sock.write(token);
+                    sock.write(Buffer.from('ff', 'hex'));
+                    buff = Buffer.from("");
+            }
 
 
+        }else{
 
+            console.log("Writing Message PDU");
+            sock.write(msgPDU);
+            sock.write(Buffer.from('ff', 'hex'));
+            buff = Buffer.from("");
 
-
-
-                })
+        }
 
 
 
 
 
 
+    })
+
+}).listen(PORT, HOST)
+
+console.log("Server listening on " + HOST + ":" + PORT);
+
+funct
 
 
-           }
-
-       }
-     
 
 
+// if (buff.toString().search('<login COOKIE="ussdgw" NODE_ID="MTNMENU_F02" PASSWORD="mtnm3nu123" RMT_SYS="uxml@ussdgw" USER="MTNMENU_F02"/>')) {
 
-   })
 
-}).listen(PORT,HOST)
+//     sock.write(token);
+//     sock.write(Buffer.from('ff', 'hex'));
 
-console.log("Server listening on "+HOST+":"+PORT);
+//     sock.on("data", info => {
 
+
+//         let hasSenWritten = sock.write(msgPDU);
+//         let hasSendPDU = sock.write(Buffer.from('ff', 'hex'));
+
+//         if (hasSenWritten) {
+//             console.log("Check PDU message sending====");
+
+//             console.log(hasSenWritten);
+
+//             sock.pause();
+
+//         }
+
+
+
+
+
+
+//     })
+
+
+// }
