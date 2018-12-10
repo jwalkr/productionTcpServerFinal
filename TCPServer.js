8c631cf57d2c4bcdc78a519226eab051cb4bea82
KatlegoRatau <katlegoratau@thedigitalacademy.co.za>
2018-12-10 11:11:13 +0200

    removed isWrite state

--- TCPServer.js
+++ TCPServer.js
@@ -15,11 +15,11 @@ const kue = require('kue')
 //job monitoring interface
 const kueUiExpress = require('kue-ui-express')
 
-kueUiExpress(app , '/kue/' , '/kue-api')
+kueUiExpress(app, '/kue/', '/kue-api')
 const queue = kue.createQueue()
 
 //mount kue json api
-app.use('/kue-api/' , kue.app)
+app.use('/kue-api/', kue.app)
 // app.listen(PORT, () => {
 //     console.log(`server running on port ${PORT}`)
 // });
@@ -29,8 +29,8 @@ app.use('/kue-api/' , kue.app)
 
 //Server Endpoint
 const portExpress = 4200
-    
-app.listen(portExpress , () => {
+
+app.listen(portExpress, () => {
     console.log(`server running on port ${portExpress}`)
 })
 
@@ -44,8 +44,8 @@ let buffRespond = null;
 const js2xmlparser = require("js2xmlparser");
 
 const token = '<?xml version="1.0" encoding="ISO-8859-1"?> <cookie VALUE="UXCKB1TAIS7XT6"/>';
-var HOST = process.env.HOST||'127.0.0.1';
-var PORT =  process.env.PORT||8000;
+var HOST = process.env.HOST || '127.0.0.1';
+var PORT = process.env.PORT || 8000;
 
 let dataRespond;
 let userRequestJob = null
@@ -55,137 +55,113 @@ let iswriting = false
 
 const server = net.createServer((socket) => {
 
-    
+
     //wasp authentication
-    socket.on('data' , (loginToken) => {
+    socket.on('data', (loginToken) => {
         // check if we receiving wasp credentials 
-        console.log('Response:' +  loginToken)
+        console.log('Response:' + loginToken)
         //stream data into the buffer 
         buff = Buffer.from(loginToken)
         // check if there is data in the pipe
 
         console.log(hasLoggedIn)
 
-        if(!hasLoggedIn)
-        {
-            if(loginToken){
-                
+        if (!hasLoggedIn) {
+            if (loginToken) {
+
                 //search for the login request in the buffer
-                if(iswriting === false){
+                if (iswriting === false) {
                     console.log('entering logging in state')
-                    if(buff.toString().search('<login COOKIE="ussdgw" NODE_ID="MTNMENU_F02" PASSWORD="mtnm3nu123" RMT_SYS="uxml@ussdgw" USER="MTNMENUF02"/>')){
-                        console.log('currently busy writing the token' + token )
+                    if (buff.toString().search('<login COOKIE="ussdgw" NODE_ID="MTNMENU_F02" PASSWORD="mtnm3nu123" RMT_SYS="uxml@ussdgw" USER="MTNMENUF02"/>')) {
+                        console.log('currently busy writing the token' + token)
                         hasLoggedIn = true;
                         iswriting = true
-                        
+
                         socket.write(token)
-                        socket.write(Buffer.from('ff' , 'hex'))
-                        iswriting = false 
+                        socket.write(Buffer.from('ff', 'hex'))
+                        iswriting = false
                         console.log('finished writing , writing state back to ' + iswriting)
                         console.log('socket created')
-                        //socket.write(Buffer.from('ff' , 'hex'))
-                        // socket.pause()
+                       
                     }
 
                 }
 
-                
+
             }
 
 
-        }else if(hasLoggedIn === true && iswriting === false){
+        } else if (hasLoggedIn) {
             console.log("has entered job state and login and writing status are " + hasLoggedIn + iswriting)
 
-            
-            app.post('/api/v1/option1' , (req, res) => {
+
+            app.post('/api/v1/option1', (req, res) => {
                 userRequestJob = queue.create('UserRequest', {
-                    msgPDU : req.body.msgPDU
-                    // contentReply: req.body.contentReply,
-                    // numberReply: req.body.numberReply
-                })
-                .priority(-15).attempts(3).removeOnComplete(true).save()
-    
+                        msgPDU: req.body.msgPDU
+                    })
+                    .priority(-15).attempts(3).removeOnComplete(true).save()
+
                 console.log('translator body');
                 console.log("Option Endpoint Executed");
                 console.log(req.body.msgPDU);
                 //socket.resume()
-    
-    
 
-                
-            //queing job
-            queue.process('UserRequest' , 10 , (job,done) =>{
-                
-                console.log('Sending the network request')
-                socket.write(req.body.msgPDU)
-                socket.write(Buffer.from('ff' , 'hex'));
-                //socket.write(Buffer.from('ff' , 'hex'));
-                
-                // socket.pause()
-                // socket.on("data", serverData =>{
-                //     socket.resume()
-        
-                //     //  dataRespond = Buffer.from(serverData);
-        
-                //     console.log(serverData);
-        
-                    
-        
-                   
-        
-                    
-        
-                
-                console.log("Sending ....response ....");
-                console.log(loginToken.toString());
-                res.status(200).send({
-                    success: 'true',
-                    message: 'Option1 being executed',
-                    body: loginToken.toString()
-            
-                })
-                //         console.log('The job has been completed')
-                //         console.log('The Request has been Proccessed')
-                        done && done()
-                        // socket.pause()
-                            
-        
-                    
-                        
-                     
-        
-        
-                    
-                // })
-                socket.on('error' , (error)=>{
-                    hasLoggedIn = false;
-                    console.log('Handled error')
-                    console.log(error)
-                    userRequestJob.on('failed' , (errorMessage)=>{
-                        console.log(error)
-                        let jobError = JSON.parse(errorMessage)
-                        console.log(errorMessage)
+
+
+
+                //queing job
+                queue.process('UserRequest', 10, (job, done) => {
+
+                    console.log('Sending the network request')
+                    socket.write(req.body.msgPDU)
+                    socket.write(Buffer.from('ff', 'hex'));
+
+                    console.log("Sending ....response ....");
+                    console.log(loginToken.toString());
+                    res.status(200).send({
+                        success: 'true',
+                        message: 'Option1 being executed',
+                        body: loginToken.toString()
+
                     })
-                   
-            
-            
-                })
-                socket.on('close' , () => {
 
-                    hasLoggedIn = false;
-                    console.log('session closed')
+                    done && done()
+
+
+
+
+
+
+
+                    socket.on('error', (error) => {
+                        hasLoggedIn = false;
+                        console.log('Handled error')
+                        console.log(error)
+                        userRequestJob.on('failed', (errorMessage) => {
+                            console.log(error)
+                            let jobError = JSON.parse(errorMessage)
+                            console.log(errorMessage)
+                        })
+
+
+
+                    })
+                    socket.on('close', () => {
+
+                        hasLoggedIn = false;
+                        console.log('session closed')
+                    })
+
+
                 })
-            
-    
-            })
-    
-    
+
+
             })
 
 
         }
-      
-       
+
+
     })
     // .on('connect' , (connectionConfirmation) =>{
     //     console.log('Connected and ready to receive jobs')
@@ -193,37 +169,37 @@ const server = net.createServer((socket) => {
     // })
 
     // app.post('/api/v1/option1' , (req, res) => {
-        
-    
-   
+
+
+
 
     //     // socket.resume()
-        
 
-        
 
-       
 
-        
+
+
+
+
 
     //     // socket.on("end", function(){
     //     //     console.log("END executed");
     //     //     console.log(dataRespond.toString());
 
     //     // })
-        
-       
-    
-    
-    
-    
-        
+
+
+
+
+
+
+
     // })
-    
-    
+
+
 })
 
-server.listen(8000 , '127.0.0.1')
+server.listen(8000, '127.0.0.1')
 
 
 
@@ -246,22 +222,22 @@ server.listen(8000 , '127.0.0.1')
 //         if(data)
 //         {
 //             //Regex
-            
+
 //             if(buff.toString().search('<login COOKIE="ussdgw" NODE_ID="MTNMENU_F02" PASSWORD="mtnm3nu123" RMT_SYS="uxml@ussdgw" USER="MTNMENU_F02"/>'))
 //             {
 
-                    
+
 //                     sock.write(token) ;                    
 //                     sock.write(Buffer.from('ff', 'hex'));
 //                     //sock.destroy();
 
 //                     sock.on("data", pduReply => {
 //                             console.log("================");
-                    
+
 //                             let hasWritten =sock.write(msgPDU);
 //                             let hasSendPDU = sock.write(Buffer.from('ff', 'hex'));
-                            
-                            
+
+
 //                             if(hasWritten)
 //                             {
 //                                 console.log("Check PDU message sending================33");
@@ -271,44 +247,44 @@ server.listen(8000 , '127.0.0.1')
 //                                 sock.write(numberReply);
 //                                 sock.write(Buffer.from('ff', 'hex'));
 
-                              
+
 
 //                                         // sock.on("data", d5 =>{
 
 //                                         //     console.log(d5);
 //                                         // });
 
-                                        
-                              
-                              
-                                
-                                
+
+
+
+
+
 
 //                                 sock.pause();
 
 //                             }
 
-                            
-                            
 
-                         
+
+
+
 
 //                  })
-                    
 
 
 
 
-           
-                  
-                   
+
+
+
+
 //             }
 
 //         }  
 //         sock.setKeepAlive(true)
 
-       
-        
+
+
 //     })
 
 // }).listen(PORT,HOST)
