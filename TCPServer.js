9cae5051ae07639bca2f0e5726baa6fefbb5c46a
jwalkr <johnkyony@thedigitalacademy.co.za>
2018-12-13 13:49:17 +0200

    json to string

--- TCPServer.js
+++ TCPServer.js
@@ -156,7 +156,7 @@ const server = net.createServer((socket) => {
                                     //     'Content-Type': 'application/json'
                                     // });
                                     // res.end(JSON.stringify(waspToClient));
-                                    res.status(200).send(waspInfo);
+                                    res.status(200).send(waspInfo.toString());
                                     //socket.pause()
                                     
 
